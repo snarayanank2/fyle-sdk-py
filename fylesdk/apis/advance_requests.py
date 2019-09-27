0e3374df3c8f0e9b490611fd3be4cae3b6d32850
Subhajit <subha@Subhajits-MacBook-Air.local>
2019-09-27 09:59:02 +0530

    Corrected some comments

--- fylesdk/apis/advance_requests.py
+++ fylesdk/apis/advance_requests.py
@@ -8,7 +8,7 @@ class AdvanceRequests(ApiBase):
     GET_ADVANCE_REQUESTS_COUNT = '/api/tpa/v1/advance_requests/count'
 
     def get(self, offset=None, limit=None, updated_at=None, exported=None, state=None, approved_at=None):
-        """Get a list of existing Advance Reports .
+        """Get a list of existing Advance requests .
 
         Parameters:
             updated_at (str): Date string in yyyy-MM-ddTHH:mm:ss.SSSZ format along with operator in RHS colon pattern.
@@ -16,8 +16,8 @@ class AdvanceRequests(ApiBase):
             offset (int): A cursor for use in pagination, offset is an object ID that defines your place in the list.
             (optional)
             limit (int): A limit on the number of objects to be returned, between 1 and 1000. (optional)
-            exported (bool): If set to true, all Advances that are exported alone will be returned. (optional)
-            state(str) : A parameter to filter expenses by the state that they're in. (optional)
+            exported (bool): If set to true, all Advance requests that are exported alone will be returned. (optional)
+            state(str) : A parameter to filter Advance requests by the state that they're in. (optional)
             approved_at(str): Date string in yyyy-MM-ddTHH:mm:ss.SSSZ format along with operator in RHS colon pattern.
             (optional)
 
@@ -34,7 +34,7 @@ class AdvanceRequests(ApiBase):
         }, AdvanceRequests.GET_ADVANCE_REQUESTS)
 
     def count(self, offset=None, limit=None, updated_at=None, exported=None, state=None, approved_at=None):
-        """Get a count of the existing Advances that match the parameters.
+        """Get a count of the existing Advance requests that match the parameters.
 
         Parameters:
             offset (int): A cursor for use in pagination, offset is an object ID that defines your place in the list.
@@ -42,10 +42,10 @@ class AdvanceRequests(ApiBase):
             limit (int): A limit on the number of objects to be returned, between 1 and 1000. (optional)
             updated_at (str): Date string in yyyy-MM-ddTHH:mm:ss.SSSZ format along with operator in RHS colon pattern.
             (optional)
-            exported (bool): If set to true, all Advances that are exported alone will be returned. (optional)
+            exported (bool): If set to true, all Advance requests  that are exported alone will be returned. (optional)
             approved_at(str): Date string in yyyy-MM-ddTHH:mm:ss.SSSZ format along with operator in RHS colon pattern.
             (optional)
-            state(str) : A parameter to filter expenses by the state that they're in. (optional)
+            state(str) : A parameter to filter Advance requests by the state that they're in. (optional)
 
         Returns:
             Count of Advance requests.
