372a42fd73827e6a5ffdeb1aa0122fb9dfd84786
Sravan Kumar <sravan.kumar@fyle.in>
2020-12-05 00:55:34 +0530

    add support to mark reimbursements as COMPLETE

--- fylesdk/apis/fyle_v1/reimbursements.py
+++ fylesdk/apis/fyle_v1/reimbursements.py
@@ -7,18 +7,30 @@ class Reimbursements(ApiBase):
     GET_REIMBURSEMENTS = '/api/tpa/v1/reimbursements'
     GET_REIMBURSEMENT_BY_ID = '/api/tpa/v1/reimbursements/{0}'
     GET_REIMBURSEMENTS_COUNT = '/api/tpa/v1/reimbursements/count'
+    POST_REIMBURSEMENTS = '/api/tpa/v1/reimbursements/complete'
+
+    def post(self, data):
+        """Complete Reimbursements in bulk.
+
+        Parameters:
+            data (list): List of Reimbursement IDs.
+
+        Returns:
+            List of updated Reimbursement IDs.
+        """
+        return self._post_request(data, Reimbursements.POST_REIMBURSEMENTS)
 
     def get(self, updated_at=None, offset=None, limit=None, exported=None):
-        """Get Reimbursments that satisfy the parameters.
+        """Get Reimbursements that satisfy the parameters.
 
         Parameters:
             updated_at (str): Date string in yyyy-MM-ddTHH:mm:ss.SSSZ format along with operator in RHS colon pattern. (optional)
             offset (int): A cursor for use in pagination, offset is an object ID that defines your place in the list. (optional)
             limit (int): A limit on the number of objects to be returned, between 1 and 1000. (optional)
-            exported (bool): If set to true, all Reimbursments that are exported alone will be returned. (optional)
+            exported (bool): If set to true, all Reimbursements that are exported alone will be returned. (optional)
 
         Returns:
-            List with dicts in Reimbursments schema.
+            List with dicts in Reimbursements schema.
         """
         return self._get_request({
             'updated_at': updated_at,
